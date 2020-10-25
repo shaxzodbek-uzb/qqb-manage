aec9d09e7020be9d95fe0421b5d0474c3d690c41
Shaxzodbek <shaxzodbek.qambaraliyev@gmail.com>
2020-10-25 14:26:07 +0500

    migration for tariffs

---
+++ database/migrations/2020_10_25_092523_make_name_text_in_tariff.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakeNameTextInTariff extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tariff', function (Blueprint $table) {
+            $table->text('name')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tariff', function (Blueprint $table) {
+            //
+        });
+    }
+}
