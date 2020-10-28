903912d0aa13886547f23cedbc8ababe8d8e09c3
Shaxzodbek <shaxzodbek.qambaraliyev@gmail.com>
2020-10-28 11:59:57 +0500

    fix table

---
+++ database/migrations/2020_10_28_065846_fix_faqs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FixFaqsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('faqs', function (Blueprint $table) {
+            $table->text('question')->change();
+            $table->text('answer')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
