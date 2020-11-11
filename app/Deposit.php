f2d7e3ee1be570dbdff8e3c7aa37d61539b13c9f
Shaxzodbek <shaxzodbek.qambaraliyev@gmail.com>
2020-11-11 20:23:43 +0500

    error

--- app/Deposit.php
+++ app/Deposit.php
@@ -5,21 +5,9 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use ClassicO\NovaMediaLibrary\Core\Model as MediaModel;
 use Spatie\Translatable\HasTranslations;
-use Illuminate\Database\Eloquent\Builder;
 
 class Deposit extends Model
 {
-    /**
-     * The "booted" method of the model.
-     *
-     * @return void
-     */
-    protected static function booted()
-    {
-        static::addGlobalScope('active', function (Builder $builder) {
-            $builder->where('active', true);
-        });
-    }
 
     use HasTranslations;
 	public $translatable = ['name','description','content', 'term', 'rate', 'summ', 'type'];
