779e7ad4c2b3ca54e0f4160e0c28acbe0046276e
Shaxzodbek <shaxzodbek.qambaraliyev@gmail.com>
2020-11-11 20:23:12 +0500

    active

--- app/DepositType.php
+++ app/DepositType.php
@@ -4,9 +4,22 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Translatable\HasTranslations;
+use Illuminate\Database\Eloquent\Builder;
 
 class DepositType extends Model
 {
+    
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::addGlobalScope('active', function (Builder $builder) {
+            $builder->where('active', true);
+        });
+    }
     use HasTranslations;
     protected $translatable = ['name'];
 }
